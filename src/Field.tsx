f40312737a4018e3b4371c38c6cd5d65108d0479
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-04-11 02:56:29 +0530

    Added add field option

--- src/Field.tsx
+++ src/Field.tsx
@@ -8,6 +8,8 @@ interface Props {
 export default function Field(props: Props) {
   if (props.type === "submit") {
     return <SubmitButton {...props} />;
+  } else if (props.type === "textarea") {
+    return <Textarea {...props} />;
   } else {
     return <RegularInput {...props} />;
   }
@@ -25,26 +27,7 @@ function RegularInput(props: Props) {
           id={props.label}
           className='p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 flex-1'
         />
-        <button
-          type='button'
-          className='ml-2 p-2 text-white bg-red-500 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600'
-          onClick={() => props.deleteFieldCB(props.id)}
-        >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='currentColor'
-            className='w-6 h-6'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
-            />
-          </svg>
-        </button>
+        <DeleteButton {...props} />
       </div>
     </>
   );
@@ -59,26 +42,52 @@ function SubmitButton(props: Props) {
       >
         {props.label}
       </button>
-      <button
-        type='button'
-        className='ml-2 p-2 text-white bg-red-500 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600'
-        onClick={() => props.deleteFieldCB(props.id)}
-      >
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          strokeWidth={1.5}
-          stroke='currentColor'
-          className='w-6 h-6'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
-          />
-        </svg>
-      </button>
+      <DeleteButton {...props} />
     </div>
+  );
+}
+
+function Textarea(props: Props) {
+  return (
+    <>
+      <label htmlFor={props.label} className='block mt-4'>
+        {props.label}
+      </label>
+      <div className='flex mt-2'>
+        <textarea
+          id={props.label}
+          className='p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 flex-1'
+        ></textarea>
+        <DeleteButton {...props} />
+      </div>
+    </>
+  );
+}
+
+function DeleteButton(props: {
+  id: number;
+  deleteFieldCB: (id: number) => void;
+}) {
+  return (
+    <button
+      type='button'
+      className='ml-2 p-2 text-white bg-red-500 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600'
+      onClick={() => props.deleteFieldCB(props.id)}
+    >
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={1.5}
+        stroke='currentColor'
+        className='w-6 h-6'
+      >
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
+        />
+      </svg>
+    </button>
   );
 }
--- src/Form.tsx
+++ src/Form.tsx
@@ -38,8 +38,24 @@ export default function Form() {
   const [fields, setFields] = useState(formFields);
 
   const deleteFieldCB = (id: number) => {
-    console.log("deleteFieldCB", id);
     setFields(fields.filter((field) => field.id !== id));
+  };
+
+  const [newFieldLabel, setNewFieldLabel] = useState("");
+  const [newFieldType, setNewFieldType] = useState("text");
+
+  const handleSubmit = () => {
+    setFields([
+      ...fields,
+      {
+        id: Number(new Date()),
+        label: newFieldLabel,
+        type: newFieldType,
+      },
+    ]);
+
+    setNewFieldLabel("");
+    setNewFieldType("text");
   };
 
   return (
@@ -47,6 +63,56 @@ export default function Form() {
       {fields.map((field) => (
         <Field key={field.id} {...field} deleteFieldCB={deleteFieldCB} />
       ))}
+
+      <div className='mt-4 border border-stone-500'></div>
+
+      <div className='mt-4'>
+        <select
+          id='type'
+          className='p-2 w-full border bg-white border-gray-300 rounded-md focus:outline-none focus:border-blue-500'
+          value={newFieldType}
+          onChange={(e) => setNewFieldType(e.target.value)}
+        >
+          <option value='text'>Text</option>
+          <option value='email'>Email</option>
+          <option value='date'>Date</option>
+          <option value='tel'>Phone</option>
+          <option value='number'>Number</option>
+          <option value='password'>Password</option>
+          <option value='textarea'>Textarea</option>
+          <option value='submit'>Submit</option>
+        </select>
+      </div>
+
+      <div className='flex mt-4'>
+        <input
+          type='text'
+          className='p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 flex-1'
+          placeholder='Label'
+          value={newFieldLabel}
+          onChange={(e) => setNewFieldLabel(e.target.value)}
+        />
+        <button
+          type='button'
+          className='ml-2 p-2 text-white bg-green-500 rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600'
+          onClick={handleSubmit}
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-6 h-6'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M12 4.5v15m7.5-7.5h-15'
+            />
+          </svg>
+        </button>
+      </div>
     </>
   );
 }
