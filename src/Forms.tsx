046cb9923a5b105ecf2986a357aae00c127f286f
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-05-11 16:59:38 +0530

    Used library for all icons

--- src/Forms.tsx
+++ src/Forms.tsx
@@ -4,7 +4,13 @@ import { Link } from "raviger";
 import { listForms } from "./utils/apiUtils";
 import { Pagination } from "./types/common";
 import ReactPaginate from "react-paginate";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  EyeIcon,
+  PencilSquareIcon,
+  PlusIcon,
+} from "@heroicons/react/24/outline";
 
 const getForms = async (
   limit: number,
@@ -44,21 +50,7 @@ export default function Forms() {
           className='flex justify-center items-center p-3 bg-blue-500 rounded text-white'
           href='/forms/create'
         >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='currentColor'
-            className='w-6 h-6'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M12 6v6m0 0v6m0-6h6m-6 0H6'
-            />
-          </svg>{" "}
-          Create Form
+          <PlusIcon className='h-6 w-6 mr-1' aria-hidden='true' /> Create Form
         </Link>
       </div>
 
@@ -104,40 +96,14 @@ function FormCard(props: { formData: Form }) {
           className='p-3 bg-blue-500 hover:bg-blue-700 rounded text-white button inline-block'
           href={`/forms/${props.formData.id}`}
         >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 24 24'
-            fill='currentColor'
-            className='w-6 h-6'
-          >
-            <path d='M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z' />
-            <path d='M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z' />
-          </svg>
+          <PencilSquareIcon className='h-6 w-6' aria-hidden='true' />
         </Link>
         <Link
           href={`/preview/${props.formData.id}`}
           target='_blank'
           className='ml-2 p-3 bg-blue-500 hover:bg-blue-700 rounded text-white'
         >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='currentColor'
-            className='w-6 h-6'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z'
-            />
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
-            />
-          </svg>
+          <EyeIcon className='h-6 w-6' aria-hidden='true' />
         </Link>
       </div>
     </div>
--- src/components/FormBuilder/CreateField.tsx
+++ src/components/FormBuilder/CreateField.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Field } from "../../types/formTypes";
+import { PlusIcon } from "@heroicons/react/24/outline";
 
 export default function CreateField(props: {
   createFieldCB: (field: Field) => void;
@@ -54,20 +55,7 @@ export default function CreateField(props: {
         type='submit'
         className='ml-2 p-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600'
       >
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          strokeWidth={1.5}
-          stroke='currentColor'
-          className='w-6 h-6'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M12 4.5v15m7.5-7.5h-15'
-          />
-        </svg>
+        <PlusIcon className='h-6 w-6' aria-hidden='true' />
       </button>
     </form>
   );
