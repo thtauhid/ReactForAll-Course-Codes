ad9c2fb3e82ccdb5b9ce6debc2539fbf85f53ce8
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-05-11 16:42:50 +0530

    implemented pagination

--- src/Forms.tsx
+++ src/Forms.tsx
@@ -3,19 +3,39 @@ import { Form } from "./types/formTypes";
 import { Link } from "raviger";
 import { listForms } from "./utils/apiUtils";
 import { Pagination } from "./types/common";
+import ReactPaginate from "react-paginate";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
 
-const getForms = async (setFormsCB: (value: Form[]) => void) => {
-  const data: Pagination<Form> = await listForms({ limit: 10, offset: 0 });
+const getForms = async (
+  limit: number,
+  offset: number,
+  setFormsCB: (value: Form[]) => void,
+  setCountCB: (value: number) => void
+) => {
+  const data: Pagination<Form> = await listForms({ limit, offset });
 
+  setCountCB(data.count);
   setFormsCB(data.results);
 };
 
 export default function Forms() {
+  const limit = 5;
+  const [offset, setOffset] = useState<number>(0);
+  const [count, setCount] = useState<number>(0);
+
   const [forms, setForms] = useState<Form[]>([]);
 
   useEffect(() => {
-    getForms(setForms);
-  }, []);
+    getForms(limit, offset, setForms, setCount);
+  }, [limit, offset]);
+
+  const pageCount = Math.ceil(count / limit);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (selectedItem: { selected: number }) => {
+    const newOffset = (selectedItem.selected * limit) % count;
+    setOffset(newOffset);
+  };
 
   return (
     <>
@@ -45,6 +65,32 @@ export default function Forms() {
       {forms.map((form) => (
         <FormCard key={form.id} formData={form} />
       ))}
+
+      <div className='flex justify-between items-center'>
+        <p>
+          Displaying {1 + offset} to{" "}
+          {limit + offset < count ? limit + offset : count} of {count}
+        </p>
+        <ReactPaginate
+          breakLabel='...'
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={1}
+          pageCount={pageCount}
+          previousLabel={
+            <ChevronLeftIcon className='h-5 w-5' aria-hidden='true' />
+          }
+          nextLabel={
+            <ChevronRightIcon className='h-5 w-5' aria-hidden='true' />
+          }
+          renderOnZeroPageCount={null}
+          containerClassName='isolate inline-flex -space-x-px rounded-md shadow-sm mt-5'
+          previousClassName='rounded-l p-2 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 hover:bg-blue-500 hover:text-white'
+          pageClassName='px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex hover:bg-blue-500 hover:text-white'
+          nextClassName='rounded-r-md p-2 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 hover:bg-blue-500 hover:text-white'
+          breakClassName='px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex hover:bg-blue-500 hover:text-white'
+          activeClassName='bg-blue-800 text-white hover:bg-blue-800 hover:text-white'
+        />
+      </div>
     </>
   );
 }
