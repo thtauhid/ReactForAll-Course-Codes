58a3dc496c72a5aecd4ab062f44e69e1e7d5faba
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-05-29 17:20:21 +0600

    added copy to clipboard option

--- src/components/FormBuilder/ShareLink.tsx
+++ src/components/FormBuilder/ShareLink.tsx
@@ -1,7 +1,11 @@
-import { EyeIcon } from "@heroicons/react/24/outline";
+import { CheckIcon, ClipboardIcon, EyeIcon } from "@heroicons/react/24/outline";
 import { Link } from "raviger";
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function ShareLink(props: { id: number }) {
+  const [copied, setCopied] = useState<boolean>(false);
+
   return (
     <div className='flex mt-4'>
       <input
@@ -10,6 +14,23 @@ export default function ShareLink(props: { id: number }) {
         value={`http://localhost:3000/preview/${props.id}`}
         readOnly
       />
+      <CopyToClipboard
+        text={`http://localhost:3000/preview/${props.id}`}
+        onCopy={() => setCopied(true)}
+      >
+        <button
+          className={`ml-2 p-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none  ${
+            copied ? "bg-green-500 hover:bg-green-600" : ""
+          }`}
+        >
+          {copied ? (
+            <CheckIcon className='h-6 w-6' aria-hidden='true' />
+          ) : (
+            <ClipboardIcon className='h-6 w-6' aria-hidden='true' />
+          )}
+        </button>
+      </CopyToClipboard>
+
       <Link
         type='button'
         className='ml-2 p-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600'
